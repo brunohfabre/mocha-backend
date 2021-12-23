34be877eb5a74608d4dae5869a2e8e6b31724eac
Bruno Fabre <bruno.hfabre@gmail.com>
2021-12-23 19:52:33 -0300

    fix find users

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,16 +1,8 @@
 import { Router } from 'express';
 
-import { prisma } from '@shared/prisma';
-
 import UsersController from '../controllers/UsersController';
 
 const usersRouter = Router();
-
-usersRouter.get('/', async (request, response) => {
-  const users = await prisma.user.findMany();
-
-  return response.json(users);
-});
 
 usersRouter.post('/', UsersController.create);
 
