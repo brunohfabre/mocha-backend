60c0bb677c55b27e52eb862db10d3b1e12734d32
Bruno Fabre <bruno.hfabre@gmail.com>
2022-01-20 22:12:00 -0300

    fix

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 
+import { Validation } from '@helpers/Validation';
+
 import { UsersController } from '../controllers/UsersController';
+import { createUserDTO } from '../dtos/createUserDTO';
 
 const usersRouter = Router();
 
-usersRouter.post('/', UsersController.create);
+usersRouter.post(
+  '/',
+  Validation.validateBody(createUserDTO),
+  UsersController.create,
+);
 
 export { usersRouter };
