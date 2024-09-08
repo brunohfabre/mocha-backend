2225e22ce7a8954814ef93615a6f21ae52d074cc
Bruno Fabre <bruno.hfabre@gmail.com>
2024-09-08 16:48:11 -0300

    add create organization

--- src/http/routes/index.ts
+++ src/http/routes/index.ts
@@ -2,6 +2,7 @@ import type { FastifyInstance } from 'fastify'
 
 import { authenticateWithGithub } from './auth/authenticate-with-github'
 import { getProfile } from './auth/get-profile'
+import { createOrganization } from './organizations/create-organization'
 import { getOrganizations } from './organizations/get-organizations'
 
 export async function routes(app: FastifyInstance) {
@@ -9,4 +10,5 @@ export async function routes(app: FastifyInstance) {
   app.register(getProfile)
 
   app.register(getOrganizations)
+  app.register(createOrganization)
 }
