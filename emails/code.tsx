3d726217bec236b15d452640e0432b3e001275ae
Bruno Fabre <bruno.hfabre@gmail.com>
2024-09-26 13:41:30 -0300

    fix code email template

--- emails/code.tsx
+++ emails/code.tsx
@@ -1,4 +1,4 @@
-import { Button, Tailwind } from "@react-email/components";
+import { Html, Text } from "@react-email/components";
 
 interface CodeEmailProps {
   code: string
@@ -6,13 +6,8 @@ interface CodeEmailProps {
 
 export function CodeEmail({ code }: CodeEmailProps) {
   return (
-    <Tailwind>
-      <Button
-        href="https://example.com"
-        className="bg-violet-500 px-3 py-2 font-medium leading-4 text-white"
-      >
-        Click me
-      </Button>
-    </Tailwind>
+    <Html lang="en" dir="ltr">
+      <Text>Verification code: {code}</Text>
+    </Html>
   )
 }
