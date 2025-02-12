2afb492bf766db4a623d3b83eff7bc4609f63afe
Bruno Fabre <bruno.hfabre@gmail.com>
2025-02-12 18:02:32 -0300

    add get profile route

--- src/http/routes/index.ts
+++ src/http/routes/index.ts
@@ -1,6 +1,9 @@
 import type { FastifyInstance } from 'fastify'
 import { authenticateFromGithubRoute } from './auth/authenticate-from-github'
+import { getProfileRoute } from './users/get-profile'
 
 export async function appRoutes(app: FastifyInstance) {
   app.register(authenticateFromGithubRoute)
+
+  app.register(getProfileRoute)
 }
